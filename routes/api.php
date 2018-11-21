adc6e512e85028effd0e9b8132d7b8ee9286a979
Scott Moore <scott.chandler.moore@gmail.com>
2018-11-21 14:31:24 -0700

    Stub out API route registration.

--- routes/api.php
+++ routes/api.php
@@ -12,7 +12,10 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::get('/', function() {
+    return response("", 200);
+});
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/products', function() {
+    return response("Successful POST to /products", 200);
 });
