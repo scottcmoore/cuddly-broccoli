103a18b0006aeda3abe4c88f43d64a7a155cd7dd
Scott Moore <scott.chandler.moore@gmail.com>
2018-11-21 17:30:20 -0700

    Allow routing and find() by sku, rather than id.

--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    // Allow finding by sku, rather than default 'id'.
+    public $primaryKey = 'sku';
+    
+    
+    /**
+     * Allow routing by sku, rather than default 'id'.
+     *
+     * @return string
+     */
+    public function getRouteKeyName()
+    {
+        return 'sku';
+    }
 }
